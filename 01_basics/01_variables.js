dd51f455cf0bdfbfe854c0361daeba0f033270d0
SamarthPadmaker <122174388+SamarthPadmaker@users.noreply.github.com>
2026-01-01 16:43:59 +0000

    var,let,cons done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail = "samarth@gmail.com"
+var accountPassword = "123"
+accountCity = "pune"
+let accountState;
+
+// accountID = 2 // not allowed coz its now allowed
+console.log(accountId);
+
+accountEmail = "samarth123@gamil.com"
+accountPassword = 1111
+accountCity = "jaipur"
+
+console.log(accountId);
+
+//console.table ka syntax always ([]).
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
