9e39f462ac9c6ca0021fa39d4a9eca90f12a3529
SamarthPadmaker <122174388+SamarthPadmaker@users.noreply.github.com>
2026-01-02 04:52:00 +0000

    dataypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //  use all JS code as newer version //
+
+// alert(3 + 3) // we are using nodejs, not browser so it give error.//
+
+console.log("sam");
+
+let name = "samarth"
+let age = 22
+let isLoggedIn = false
+
+/* number => 2 power 53
+   bigint = 
+   string => ""
+   boolean => true/false
+   null => standalone value (type)
+   undefined => value not defined like (let state;)
+   symbols => unique
+
+   object
+
+   */
+
+   console.log(typeof undefined); // o/p: undefined //
+   console.log(typeof null); // o/p: object //
+   
+   
